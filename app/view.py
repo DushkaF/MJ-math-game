fa32a72965982caf2c57e3b1d99e5d19b20fe21e
Dushko Fedor <fdsaq1234@ya.ru>
2020-07-15 01:13:11 +0300

    Made adaptive

--- app/view.py
+++ app/view.py
@@ -1,6 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
 
 @app.route("/")
 def index():
+    print()
     return render_template("index.html")
