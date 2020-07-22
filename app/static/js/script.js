69226e86c156f7bc76c0e6c2eeaff48c891ca9fa
Dushko Fedor <fdsaq1234@ya.ru>
2020-07-23 01:26:50 +0300

    add main page

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,5 +1,5 @@
-window.onload = function () { // Событие страница загружена
-    document.getElementById('D').onclick = function () { Hidden() };
+/*window.onload = function() { // Событие страница загружена
+    document.getElementById('D').onclick = function() { Hidden() };
 }
 
 function Hidden() {
@@ -13,4 +13,36 @@ function sendRequest() {
 
     request.open('GET', '/request');
     request.send("Hello AJAX");
+}*/
+
+$(document).ready(function () {
+  console.log("Ready");
+
+  $(".navClickable").click(function () {
+    return loadNavDiv($(this));
+  });
+});
+
+function loadNavDiv(doc) {
+  var toLoad = doc.attr("href") + "#navContent";
+  console.log(toLoad);
+  $("#navContent").hide("fast", loadContent);
+  function loadContent() {
+    $.get(
+      toLoad,
+      function (data) {
+        $("#navContent").replaceWith(data);
+      },
+      "",
+      showNewContent()
+    );
+  }
+  function showNewContent() {
+    $("#navContent").show("normal");
+  }
+  return false;
 }
+
+$(window).on("load", function () {
+  $("#firstLook").trigger("click");
+});
--- app/view.py
+++ app/view.py
@@ -5,3 +5,13 @@ from flask import render_template, request
 def index():
     print()
     return render_template("index.html")
+
+@app.route("/game")
+def game():
+    print()
+    return render_template("game.html")
+
+'''@app.route("/navbar_content/aboutGame.html")
+def nav():
+    print()
+    return render_template("/navbar_content/aboutGame.html")'''
